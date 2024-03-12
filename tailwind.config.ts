0fa58b11d4e6ca20f56dc3b088986ccd4197e6b0
E Joon Ko <ejoonko@Es-MacBook-Pro.lan>
2024-03-12 15:59:36 +0100

    created styling specifications

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'umain-stroke': '#000000',
+        'umain-off-white': '#FAFAFA',
+        'umain-green': '#00703A',
       },
+      fontSize: {
+        '40px': '40px',
+      }
     },
   },
   plugins: [],
